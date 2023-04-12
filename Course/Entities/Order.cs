957cabb4bb4cdca35fd067ed539490bec7b92529
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2023-04-12 16:42:40 -0300

    Add project files.

---
+++ Course/Entities/Order.cs
@@ -0,0 +1,63 @@
+﻿
+using Course.Entities.Enums;
+using System.Globalization;
+using System.Text;
+
+namespace Course.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        List<OrderItem> Items = new List<OrderItem>();
+
+        public Order() { }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void addItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void removeItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.subTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMMARY:");
+            sb.Append("Order moment: ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.Append("Order status: ");
+            sb.AppendLine(Status.ToString());
+            sb.Append("Client: ");
+            sb.AppendLine($"{Client.Name} ({Client.BirthDate.ToString("dd/MM/yyyy")}) - {Client.Email}");
+            sb.AppendLine("Order items:");
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine($"{item.Product.Name}, ${item.Product.Price.ToString("F2", CultureInfo.InvariantCulture)}, Quantity: {item.Quantity}, Subtotal: {item.subTotal().ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            sb.AppendLine($"Total price: {total().ToString("F2", CultureInfo.InvariantCulture)}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,50 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+using System.Globalization;
+
+namespace Aulas
+{
+    class Aulas
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth Date (DD/MM/YYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items to this order? ");
+            int items = int.Parse(Console.ReadLine());
+            Order order = new Order(
+                DateTime.Now, 
+                status, 
+                new Client(name, email, date)
+            );
+
+            for (int i = 1; i <= items; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string prodName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double prodPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                order.addItem(new OrderItem(
+                    quantity, 
+                    prodPrice, 
+                    new Product(prodName, prodPrice)
+                ));
+            }
+
+            Console.WriteLine(order);
+        }
+    }
+}
